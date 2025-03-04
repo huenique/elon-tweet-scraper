0bc56cf3612f1455b67a87b494afd7b05a8b14da
Hju Kneyck Flores <hjucode@gmail.com>
2025-03-04 15:58:54 +0800

    feat: add `twscrape` result to final output

--- elon_tweet_scraper/__main__.py
+++ elon_tweet_scraper/__main__.py
@@ -4,9 +4,11 @@ import os
 
 import click
 from dotenv import load_dotenv
+from httpx import Response
 from twikit.client.client import Client as TwiKit
+from twikit.tweet import Tweet
+from twscrape.api import API
 
-# Load environment variables
 load_dotenv()
 
 
@@ -18,30 +20,53 @@ def get_env_var(name: str) -> str:
     return value
 
 
+async def fetch_latest_tweet_details(api: API, tweet: Tweet) -> Response:
+    return await api.tweet_details_raw(tweet.id) or Response(  # type: ignore
+        status_code=200, content=b"{}"
+    )
+
+
 async def fetch_latest_tweet():
     """Fetches the latest tweet from Elon Musk and returns it as a dictionary."""
     tk = TwiKit()
+    api = API()
+    username = get_env_var("TWITTER_USERNAME")
+    email = get_env_var("TWITTER_EMAIL")
+    password = get_env_var("TWITTER_PASSWORD")
+    cookies_file = get_env_var("TWITTER_COOKIES_FILE")
 
     await tk.login(  # type: ignore
-        auth_info_1=get_env_var("TWITTER_USERNAME"),
-        auth_info_2=get_env_var("TWITTER_EMAIL"),
-        password=get_env_var("TWITTER_PASSWORD"),
-        cookies_file=get_env_var("TWITTER_COOKIES_FILE"),
+        auth_info_1=username,
+        auth_info_2=password,
+        password=password,
+        cookies_file=cookies_file,
     )
 
-    latest_tweet = await tk.get_user_tweets("44196397", "Tweets", count=1)
+    with open(cookies_file, "r") as file:
+        cookies = json.dumps(json.load(file))
 
+    await api.pool.add_account(  # type: ignore
+        username=username,
+        password=password,
+        email=email,
+        email_password=password,
+        cookies=cookies,
+    )
+
+    await api.pool.login_all()  # type: ignore
+
+    latest_tweet = await tk.get_user_tweets("44196397", "Tweets", count=1)
     if latest_tweet:
         tweet = latest_tweet[0]
-
-        # Convert tweet object to dictionary (extracting all attributes)
-        tweet_data = {
+        summary = {
             attr: getattr(tweet, attr)
             for attr in dir(tweet)
             if not attr.startswith("_") and not callable(getattr(tweet, attr))
         }
 
-        return tweet_data
+        summary["details"] = fetch_latest_tweet_details(api, tweet)
+
+        return summary
     else:
         raise ValueError("❌ No tweets found.")
 
